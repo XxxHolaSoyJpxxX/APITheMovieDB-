9bde92ef7e02efdccf989b50ab25fddecb5d3e5e
JP <juanpablo.conquistador@gmail.com>
2025-02-07 11:05:48 -0600

    Movie class constructor

--- Movie.py
+++ Movie.py
@@ -1,1 +1,10 @@
-print('hello wolrd')
+class MOVIE :
+    
+    def __intit__(self,title:str='',overview:str='',lenguage:str='',movieRatings:str='',releaseDate:str='') -> None:
+        self.title = title
+        self.overview = overview
+        self.lenguage = lenguage
+        self.movieRatings = movieRatings
+        self.releaseDate = releaseDate
+        
+    
