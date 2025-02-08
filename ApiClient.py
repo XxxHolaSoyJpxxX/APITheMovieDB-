79d4b7321e5e923ed5c085ae1964d39682f76c15
JP <juanpablo.conquistador@gmail.com>
2025-02-07 20:58:17 -0600

    Apiclient and Format for Movie

--- ApiClient.py
+++ ApiClient.py
@@ -1,6 +1,14 @@
 import requests
-from Movie import Movie
 import json
+
+
+class Movie:
+    def __init__(self, title: str = '', overview: str = '', language: str = '', movie_ratings: str = '', release_date: str = '') -> None:
+        self.title = title
+        self.overview = overview
+        self.language = language
+        self.movie_ratings = movie_ratings
+        self.release_date = release_date
 
 
 class ApiClient :
